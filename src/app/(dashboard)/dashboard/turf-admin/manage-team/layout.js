8fe3572ddf416d6ba1ca140b2b6d64e4cf00fa9c
AH-Araf <arafathossain.baiust@gmail.com>
2026-04-28 21:11:32 +0600

    Done

---
+++ src/app/(dashboard)/dashboard/turf-admin/manage-team/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Manage team | TurfAdmin | Turffin",
+  description: "Manage staff accounts, access roles, and permissions for your turf organization.",
+};
+
+export default function Layout({ children }) {
+  return children;
+}
